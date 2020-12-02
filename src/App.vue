c6cb95725daa0e6b11ada74d8888fc948cf8cdad
k.m.jalal hossain <jalal.khan981@gmail.com>
2020-12-01 22:03:53 -0800

    vujs todo

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<todo-list list-name="My to-dos"/>
+
+</template>
+
+<script>
+import TodoList from './Components/TodoList';
+export default {
+  components:{
+   TodoList
+  }
+}
+</script>
+<!--global style-->
+<style>
+body{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/Components/CreateTodo.vue
@@ -0,0 +1,30 @@
+<template>
+  <form class="col-12 col-sm-10 col-md-8 col-lg-6" @submit.prevent="addTodo">
+    <input type="text" class="form-control" placeholder="New Todo" v-model="newTodo">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <button
+            class="btn btn-primary" @click="addTodo">Submit!
+        </button>
+      </div>
+    </div>
+  </form>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      newTodo:''
+    }
+  },
+  methods:{
+    addTodo(){
+      if(this.newTodo.length>0){
+        this.$emit('on-new-todo',this.newTodo);
+      }
+      this.newTodo=''
+    }
+  }
+}
+</script>
+<style scoped></style>
